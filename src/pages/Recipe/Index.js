285fead13096aa4bbc4091ed2cfede00d292a18a
MBorowiecki <borowieckimateusz1@gmail.com>
2020-05-17 00:44:03 +0200

    Recipe page UI. Completed part of Recipe page UI (only with data, which can be currently downloaded from and uploaded to database).

--- src/pages/Recipe/Index.js
+++ src/pages/Recipe/Index.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import {
-    useParams
+    useParams,
+    useHistory
 } from 'react-router-dom'
 import * as firebase from 'firebase'
 import 'firebase/firestore'
 import firebaseConfig from '../../config/firebase'
+import {
+    ArrowBack
+} from '@material-ui/icons'
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     background-color: #d5e6e4;
     min-height: 100vh;
+    font-family: 'Muli', sans-serif;
 `
 
 const Content = styled.div`
@@ -28,22 +33,26 @@ const FirstContainer = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column;
+    background-color: #ffffff;
+    box-shadow: 4px 0px 15px #00000033;
+    max-width: 33vw;
+    max-height: 100vh;
 `
 
 const SecondContainer = styled.div`
-    flex: 1;
+    flex: 2;
 `
 
 const Image = styled.img`
     flex: 1;
     object-fit: cover;
-    max-height: 50%;
+    max-height: 40%;
 `
 
 const MetaInfo = styled.div`
     display: flex;
     flex-direction: row;
-    background-color: #cacaca;
+    background-color: #dadada;
     margin: 16px;
     padding: 8px;
     border-radius: 10px;
@@ -85,17 +94,111 @@ const DescriptionIngredients = styled.div`
     flex-direction: row;
     margin: 16px;
 
-    .ingredients{
-        font-size: 24px;
-        color: #000000;
+    .left{
         flex: 1;
+        display: flex;
+        flex-direction: column;
+        border-right: 1px solid #888888;
+        padding: 8px 16px 8px 8px;
+
+        .label{
+            font-size: 24px;
+            color: #4a4a4a;
+            margin-bottom: 8px;
+            font-weight: 300;
+        }
+
+        .ingredients{
+            flex-grow: 1;
+            font-size: 20px;
+            color: #101010;
+            
+            ul{
+                margin: 0px;
+                padding-left: 20px;
+                overflow-y: scroll;
+            }
+        }
     }
 
-    .description{
-        margin: 8px;
-        font-size: 24px;
-        color: #000000;
+    .right{
         flex: 1;
+        margin: 8px 8px 8px 16px;
+        display: flex;
+        flex-direction: column;
+
+        .label{
+            font-size: 24px;
+            color: #4a4a4a;
+            margin-bottom: 8px;
+            font-weight: 300;
+        }
+
+        .description{
+            flex-grow: 1;
+            font-size: 20px;
+            color: #101010;
+        }
+    }
+`
+
+const StepsToProduce = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 16px;
+    padding: 16px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    box-shadow: 0px 4px 15px #00000033;
+
+    .label{
+        color: #4a4a4a;
+        font-size: 24px;
+        margin-bottom: 8px;
+        font-weight: 300;
+    }
+
+    .text{
+        color: #101010;
+        flex-grow: 1;
+        font-size: 24px;
+
+        ul{
+            margin: 0px;
+            padding-left: 24px;
+        }
+    }
+`
+
+const BackButton = styled.div`
+    position: fixed;
+    top: 16px;
+    left: 16px;
+    padding: 8px;
+    background-color: #00000033;
+    border: 1px solid #ffffff;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    box-shadow: 0px 4px 10px #00000022;
+    user-select: none;
+    transition: background-color 100ms, box-shadow 100ms;
+
+    :hover{
+        cursor: pointer;
+        background-color: #0000003f;
+        box-shadow: 0px 4px 15px #0000003f;
+    }
+
+    .icon{
+        font-size: 24px;
+        color: #ffffff;
+    }
+
+    .text{
+        font-size: 20px;
+        color: #ffffff;
     }
 `
 
@@ -105,6 +208,7 @@ const Index = () => {
     }
 
     const { id } = useParams();
+    const history = useHistory();
     const db = firebase.firestore();
     const [recipe, setRecipe] = useState({});
 
@@ -112,10 +216,18 @@ const Index = () => {
         db.collection('recipes').doc(id).onSnapshot(snap => {
             setRecipe(snap.data());
         })
+
+        console.log(recipe.ingredients)
     })
 
     return(
         <Container>
+            <BackButton
+                onClick={() => history.goBack()}
+            >
+                <ArrowBack className="icon" />
+                <span className="text">Go back</span>
+            </BackButton>
             <Content>
                 <FirstContainer>
                     <Image src={recipe.image} />
@@ -133,16 +245,38 @@ const Index = () => {
                         </div>
                     </MetaInfo>
                     <DescriptionIngredients>
-                        <span className="ingredients">
-                            {recipe.ingredients}
-                        </span>
-                        <span className="description">
-                            {recipe.shortDescription}
-                        </span>
+                        <div className="left">
+                            <span className="label">Ingredients</span>
+                            <span className="ingredients">
+                                <ul>
+                                    {recipe.ingredients && recipe.ingredients.split('\n').map(ingredient => {
+                                        return(<li>{ingredient}</li>)
+                                    })}
+                                </ul>
+                            </span>
+                        </div>
+                        <div className="right">
+                            <span className="label">Description</span>
+                            <span className="description">
+                                {recipe.shortDescription}
+                            </span>
+                        </div>
                     </DescriptionIngredients>
                 </FirstContainer>
                 <SecondContainer>
-
+                    <StepsToProduce>
+                        <span className="label">
+                            Steps to produce
+                        </span>
+                        <span className="text">
+                            <ul>
+                                {recipe.stepsToProduce && recipe.stepsToProduce.split('\n').map(step => 
+                                <li>
+                                    {step}
+                                </li>)}
+                            </ul>
+                        </span>
+                    </StepsToProduce>
                 </SecondContainer>
             </Content>
         </Container>
